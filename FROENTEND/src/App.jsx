a6f403083aca1e84fc66928a9d895fb39ec6519e
Prashant Raut <prashantraut1308@gmail.com>
2025-02-19 19:28:05 +0530

    solved app.jsx issue

--- FROENTEND/src/App.jsx
+++ FROENTEND/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import ChatUi from './components/ChatUi'
-import JobDescriptionForm from './components/JobDescriptionForm'
-import ChaiApp from './components/ChaiUi'
+
 
 function App() {
 
   return (
     <>
-    {/* <JobDescriptionForm/> */}
       <ChatUi/>
-      {/* <ChaiApp/> */}
     </>
   )
 }
